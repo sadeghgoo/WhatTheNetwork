2f068efd466b0163699a922d2b0464d988b3fc35
sadeghgoo <sadeghitunes2@gmail.com>
2020-07-29 11:33:06 +0430

    Initial commit

---
+++ Example/WhatTheNetwork/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WhatTheNetwork
+//
+//  Created by sadeghgoo on 07/29/2020.
+//  Copyright (c) 2020 sadeghgoo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
